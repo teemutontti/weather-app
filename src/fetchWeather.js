ea47a9facd39688927bc2a8cac7ee64e2254ad66
Mikko Pasanen <pasanemikko@gmail.com>
2023-10-06 18:14:17 +0300

    src/fetchWeather.js: (fetchWeather): Add new variable 'url'. This variable holds the url where the API data is fetched. Url contains the global 'apiKey' variable and 'city' parameter

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -1,3 +1,5 @@
 const apiKey = "e15e748acfc2424f9df135245232809";
 
-function fetchWeather(city) {}
+function fetchWeather(city) {
+  let url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3`;
+}
