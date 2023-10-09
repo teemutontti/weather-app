856ddfcaff1d8ecf873c762992f10306e774684a
Mikko Pasanen <pasanemikko@gmail.com>
2023-10-09 19:48:07 +0300

    src/weather.js: (fetchWeather): Change function to be async function

--- src/weather.js
+++ src/weather.js
@@ -3,7 +3,7 @@ const apiKey = "e15e748acfc2424f9df135245232809";
 //Function for fetching info from html input
 
 //Function for fetching weather
-function fetchWeather(city) {
+async function fetchWeather(city) {
   let url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3`;
 }
 
