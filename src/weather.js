dd92b6a26d8b2c13c2909a903466eac7faed3e79
Mikko Pasanen <pasanemikko@gmail.com>
2023-10-09 19:47:05 +0300

    src/weather.js: Add comments to separate function positions

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,16 @@
 const apiKey = "e15e748acfc2424f9df135245232809";
 
+//Function for fetching info from html input
+
+//Function for fetching weather
 function fetchWeather(city) {
   let url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3`;
 }
+
+//Function for parseing fetched data
+
+//Function for returning current weather to html
+
+//Function for returning tomorrows weather to html
+
+//Function for returning day after tomorrows weather to html
